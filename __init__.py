dd063c8abaaf911592435ceb9d103262eceff5a4
暗灵QWQ <lan357351@gmail.com>
2026-05-16 20:32:45 +0800

    发布 4.3.8：修复直出形态键 fallback 逻辑与 EFMI LOD 前缀下的 Position 文件查找

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ bl_info = {
     "name": "TheHerta4",
     "description": "Blender Plugin of SSMT4",
     "blender": (4, 5, 0),
-    "version": (4, 3, 7),
+    "version": (4, 3, 8),
     "location": "View3D",
     "category": "Generic"
 }
