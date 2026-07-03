20954c62ffc8919823146e71a4932e7de7d9955f
暗灵QWQ <lan357351@gmail.com>
2026-07-03 16:06:24 +0800

    fix(version): sync panel version text for v4.4.14

--- toolkit/ui_panel_animation.py
+++ toolkit/ui_panel_animation.py
@@ -15,7 +15,7 @@ class ATP_PT_MainPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="版本: 4.4.8")
+        layout.label(text="版本: 4.4.14")
 
 
 class ATP_PT_ShapeKeyTools(bpy.types.Panel):
